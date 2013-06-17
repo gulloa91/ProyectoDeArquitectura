bfbf5a176265ff50c35db1dcb67d3fd9157d9dcf
Gabriel Ulloa <gulloa91@gmail.com>
2013-06-17 13:04:48 -0600

    Animación completa

--- Website/js/animacion_funciones.js
+++ Website/js/animacion_funciones.js
@@ -1,6 +1,6 @@
 // Programas:
-// 1. Ciclo:
-var programa1 = [["0", "MOV", "0, R0"], ["2", "MOV" ,"R0, R1"], ["4", "MOV", "2, R1"], ["6", "MOV", "R1, R2"], ["8", "ADD", "1, R0"], ["10", "MOV" ,"R0, R2"], ["12", "MUL", "R1, R0"], ["14", "MOV", "R1, R2"], ["16", "CMP", "256, R0"], ["18", "BLE", "8"], ["8", "ADD", "1, R0"], ["10", "MOV" ,"R0, R2"], ["12", "MUL", "R1, R0"], ["14", "MOV", "R1, R2"], ["16", "CMP", "256, R0"], ["18", "BLE", "8"], ["8", "ADD", "1, R0"], ["10", "MOV" ,"R0, R2"], ["12", "MUL", "R1, R0"], ["14", "MOV", "R1, R2"], ["16", "CMP", "256, R0"], ["18", "BLE", "8"], ["8", "ADD", "1, R0"], ["10", "MOV" ,"R0, R2"], ["12", "MUL", "R1, R0"], ["14", "MOV", "R1, R2"], ["16", "CMP", "256, R0"], ["18", "BLE", "8"], ["8", "ADD", "1, R0"], ["10", "MOV" ,"R0, R2"], ["12", "MUL", "R1, R0"], ["14", "MOV", "R1, R2"], ["16", "CMP", "256, R0"], ["18", "BLE", "8"], ["8", "ADD", "1, R0"], ["10", "MOV" ,"R0, R2"], ["12", "MUL", "R1, R0"], ["14", "MOV", "R1, R2"], ["16", "CMP", "256, R0"], ["18", "BLE", "8"], ["8", "ADD", "1, R0"], ["10", "MOV" ,"R0, R2"], ["12", "MUL", "R1, R0"], ["14", "MOV", "R1, R2"], ["16", "CMP", "256, R0"], ["18", "BLE", "8"], ["20", "STP", "0"]];
+// 1. Ciclo - 6 iteraciones:
+var programa1 = [["0", "MOV", "0, R0"], ["2", "MOV" ,"R0, R1"], ["4", "MOV", "2, R1"], ["6", "MOV", "R1, R2"], ["8", "ADD", "1, R0"], ["10", "MOV" ,"R0, R2"], ["12", "MUL", "R1, R0"], ["14", "MOV", "R1, R2"], ["16", "CMP", "256, R0"], ["18", "BLE", "8"], ["8", "ADD", "1, R0"], ["10", "MOV" ,"R0, R2"], ["12", "MUL", "R1, R0"], ["14", "MOV", "R1, R2"], ["16", "CMP", "256, R0"], ["18", "BLE", "8"], ["8", "ADD", "1, R0"], ["10", "MOV" ,"R0, R2"], ["12", "MUL", "R1, R0"], ["14", "MOV", "R1, R2"], ["16", "CMP", "256, R0"], ["18", "BLE", "8"], ["8", "ADD", "1, R0"], ["10", "MOV" ,"R0, R2"], ["12", "MUL", "R1, R0"], ["14", "MOV", "R1, R2"], ["16", "CMP", "256, R0"], ["18", "BLE", "8"], ["8", "ADD", "1, R0"], ["10", "MOV" ,"R0, R2"], ["12", "MUL", "R1, R0"], ["14", "MOV", "R1, R2"], ["16", "CMP", "256, R0"], ["18", "BLE", "8"], ["8", "ADD", "1, R0"], ["10", "MOV" ,"R0, R2"], ["12", "MUL", "R1, R0"], ["14", "MOV", "R1, R2"], ["16", "CMP", "256, R0"], ["18", "BLE", "8"], ["8", "ADD", "1, R0"], ["10", "MOV" ,"R0, R2"], ["12", "MUL", "R1, R0"], ["14", "MOV", "R1, R2"], ["16", "CMP", "256, R0"], ["18", "BLE", "8"], ["8", "ADD", "1, R0"], ["10", "MOV" ,"R0, R2"], ["12", "MUL", "R1, R0"], ["14", "MOV", "R1, R2"], ["16", "CMP", "256, R0"], ["18", "BLE", "8"], ["20", "STP", "0"]];
 
 var play = false;
 
@@ -36,12 +36,22 @@ function reproduccion(){
 
 function crearTabla( tam ){
 	var tabla = $("#tabla_dinamica");
-	tabla.empty();
-	var rows = "<tr><th>Pos.</th><th>Direccion</th><th>Instr.</th></tr>";
-	for(var i = 0; i < tam; i ++) {
-		rows += "<tr><td>" + i + "</td><td id='dir_"+ i +"'>-</td><td id='inst_"+ i +"'>-</td></tr>";
+	if( $("#tam_bloque").val() == 1 ) { // Bloque 1 Word
+		tabla.empty();
+		var rows = "<tr><th>Pos.</th><th>Direccion</th><th>Instr.</th></tr>";
+		for(var i = 0; i < tam; i ++) {
+			rows += "<tr><td>" + i + "</td><td id='dir_"+ i +"'>-</td><td id='inst_"+ i +"'>-</td></tr>";
+		}
+		tabla.append(rows);
+		
+	} else { // Bloque 4 Words
+		tabla.empty();
+		var rows = "<tr><th>Tag</th><th>Direccion</th><th>Instr.</th></tr>";
+		for(var i = 0; i < tam; i ++) {
+			rows += "<tr><td>-</td><td id='dir_"+ i +"'>-</td><td id='inst_"+ i +"'>-</td></tr>";
+		}
+		tabla.append(rows);
 	}
-	tabla.append(rows);
 }
 
 function volverAEmpezar(){
@@ -95,7 +105,7 @@ function borrar_instrucciones(){
 	cacheColoresOriginales();
 	$("#paso").text("0");
 	$("#hits").text("0");
-	$("#ratio").text("0%");
+	$("#ratio").text("0");
 	$("#instrucciones").val("");
 	play = false;
 	$("#btnReproducir").html("Reproducir <i class='icon-play'></i>");
@@ -103,9 +113,10 @@ function borrar_instrucciones(){
 
 function analizarInstruccion( espacioVacio, direccion, instruccion ){
 	cacheColoresOriginales();
-	if( espacioVacio != -1 ) { // Existe un espacio vacío en Cache
-		$("#dir_" + espacioVacio).text( direccion );
-		$("#inst_" + espacioVacio).text( instruccion );
+	if( espacioVacio != -1 && estaEnCache(direccion) == -1 ) { // Existe un espacio vacío en Cache
+		//$("#dir_" + espacioVacio).text( direccion );
+		//$("#inst_" + espacioVacio).text( instruccion );
+		agregarBloquesEnCacheConVacio( $("#tam_bloque").val(), espacioVacio, direccion, instruccion ); // Optimización
 		
 	} else { // Aplicar algoritmo para buscar un espacio
 		var indice = estaEnCache(direccion);
@@ -125,7 +136,9 @@ function analizarInstruccion( espacioVacio, direccion, instruccion ){
 			});
 			
 		} else { // No está en Cache, añadir
+			
 			indice = posicionAleatoria();
+			/*
 			$("#dir_" + indice).text( direccion );
 			$("#dir_" + indice).css({ 
 				"background": "#414141",
@@ -136,12 +149,14 @@ function analizarInstruccion( espacioVacio, direccion, instruccion ){
 				"background": "#414141",
 				"color":"#FFF"
 			});
+			*/
+			agregarBloquesEnCacheSinVacio( $("#tam_bloque").val(), indice, direccion, instruccion )
 		}
 	}
 }
 
 function borrarValoresTabla(){
-	var pasos = parseInt( $("#paso").text() );
+	var pasos = $("#tam_cache").val();
 	for( var i = 0; i < pasos; i++ ){
 		$("#dir_" + i).text( "-" );
 		$("#inst_" + i).text( "-" );
@@ -149,7 +164,7 @@ function borrarValoresTabla(){
 }
 
 function cacheColoresOriginales(){
-	var pasos = parseInt( $("#paso").text() );
+	var pasos = $("#tam_cache").val();
 	for( var i = 0; i < pasos; i++ ){
 		$("#dir_" + i).css({ 
 			"background": "none",
@@ -162,10 +177,62 @@ function cacheColoresOriginales(){
 	}
 }
 
+/***************** FUNCIONES OPTIMIZACIÓN **************/
+// Con espacio vacío
+function agregarBloquesEnCacheConVacio( tam, espacioVacio, direccion, instruccion ) {
+	var paso = parseInt( $("#paso").text() ) + 1;
+	
+	// Agregar primer bloque
+	$("#dir_" + espacioVacio).text( direccion );
+	$("#inst_" + espacioVacio).text( instruccion );
+	
+	// Agregar siguientes bloques
+	for( var i = 1; i < tam; i++ ) {
+		espacioVacio = ++espacioVacio;
+		
+		$("#dir_" + espacioVacio).text( programa1[paso][0] ).css({ 
+			"background": "#414141",
+			"color":"#FFF"
+		});;
+		$("#inst_" + espacioVacio).text( programa1[paso][1] ).css({ 
+			"background": "#414141",
+			"color":"#FFF"
+		});;
+		
+		paso = ++paso;
+	}
+}
+
+// Sin espacio vacío
+function agregarBloquesEnCacheSinVacio( tam, indice, direccion, instruccion ) {
+	var paso = parseInt( $("#paso").text() );
+	
+	// Primero buscar posicion inicial:
+	while( (indice % tam) != 0 ) { // No es límite
+		indice = --indice;
+	}
+	
+	// Agregar bloques
+	for( var i = 0; i < parseInt(tam); i++ ) {
+		
+		$("#dir_" + indice).text( programa1[paso][0] ).css({ 
+			"background": "#414141",
+			"color":"#FFF"
+		});;
+		$("#inst_" + indice).text( programa1[paso][1] ).css({ 
+			"background": "#414141",
+			"color":"#FFF"
+		});;
+		
+		paso = ++paso;
+		indice = ++indice;
+	}
+}
+
 /******************  BÚSQUEDAS *****************/
 // Buscar direccion en Cache
 function estaEnCache( direccion ) {
-	var pasos = parseInt( $("#paso").text() );
+	var pasos = getCacheElementQty();
 	var esta = -1;
 	for( var i = 0; i < pasos; i++ ){
 		if( $("#dir_" + i).text() == direccion ) { // Encontrado
@@ -194,4 +261,17 @@ function posicionAleatoria() {
 	var rango = $("#tam_cache").val();
 	// Retorna de 0 a Rango
 	return Math.floor( ( Math.random() * rango ));
+}
+
+function getCacheElementQty(){
+	var espacios = $("#tam_cache").val();
+	var cnt = 0;
+	for( var i = 0; i < espacios; i++ ){
+		if( $("#dir_" + i).text() != "-" ) {
+			cnt = ++cnt;
+		} else {
+			break;
+		}
+	}
+	return cnt;
 }
